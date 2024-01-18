e4536e5edcfeb37c929527ca3543b0a4d72227f5
JupperTV <fmmartyn10@gmail.com>
2024-01-18 10:45:33 +0100

    Args, help, and testquizzes

--- main.cpp
+++ main.cpp
@@ -9,19 +9,24 @@
 
 #include "include/json.hpp"
 
+
+constexpr int ARGC_WHEN_NO_ARGUMENTS = 1;
+
+
 std::vector<std::string> mixItUp(std::vector<std::string> vec) {
     auto rng = std::default_random_engine {};
     std::shuffle(std::begin(vec), std::end(vec), rng);
     return vec;
 }
 
-// TODO
 bool OSIsWindows(){
-    #ifdef _WIN32 || _WIN64 || __CYGWIN__
+#ifdef _WIN32 // _WIN64 isn't checked because if _WIN64 is defined then _WIN32 is defined too
     return true;
-    #else
+#elif __CYGWIN__ 
+    return true;
+#else
     return false;
-    #endif
+#endif
 }
 
 enum QuestionType {
@@ -65,12 +70,45 @@ public:
 
 using json = nlohmann::json;
 
-void UseTestQuizzes() {
-
+// Can't name the parameter "json" because json is already defined as a type above
+// The Json needs to be a valid quiz
+bool JsonIsValidQuiz(json jsonObject) {
+    // TODO
+    return false;
 }
+
+void UseTestQuizzes() {
+    // TODO
+    std::string exampleQuizPath  = "./examplequizzes";
+    json exampleQuiz1 = json::parse(std::ifstream(exampleQuizPath + "/quiz1.json"));
+    json exampleQuiz2 = json::parse(std::ifstream(exampleQuizPath + "/quiz2.json"));
+    
+    for(auto Question : exampleQuiz1) {
+        for(auto Answer : Question){
+            // TODO: Find out, how to display the Keys
+            std::cout << Answer << "\n";
+        }
+        std::cout << "\n";
+    }
+}
+
+
+
+void CheckArgv(int argc, char **argv) {
+    if (argv[1] == "-h"){
+        // TODO: Make a better help page
+        std::cout << "Usage: ./quizzies [<path>]\n\nPlay a Quiz with the json ";
+    }
+}
+
 
 int main(int argc, char **argv) {
-    json quiz = json::parse(std::ifstream("layoutForQuestions.json"));
-    // std::filesystem::
+    if (argc > ARGC_WHEN_NO_ARGUMENTS) {
+        CheckArgv(argc, argv);
+    } else {
+        UseTestQuizzes();
+    }
+
     return 0;
 }
+
